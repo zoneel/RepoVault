cf7d2e97b39e5374704f2910c7fbd5c307564e5f
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-10-06 08:47:29 +0200

    updated RepositoryDTO.cs

--- RepoVault.Domain/Entities/RepositoryDTO.cs
+++ RepoVault.Domain/Entities/RepositoryDTO.cs
@@ -2,7 +2,24 @@
 
 public class RepositoryDTO
 {
+    public RepositoryDTO(long id, string name, string description, string language, DateTimeOffset createdAt, DateTimeOffset updatedAt, long size, long openIssuesCount)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        Language = language;
+        CreatedAt = createdAt;
+        UpdatedAt = updatedAt;
+        Size = size;
+        OpenIssuesCount = openIssuesCount;
+    }
+
     public long Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public string Language { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset UpdatedAt { get; set; }
+    public long Size { get; set; }
+    public long OpenIssuesCount { get; set; }
 }
