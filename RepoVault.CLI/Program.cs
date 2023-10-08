7a6a6e6da6a9643be0c915564d91e7b828a27adf
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-10-08 23:42:07 +0200

    refactored user UI

--- RepoVault.CLI/Program.cs
+++ RepoVault.CLI/Program.cs
@@ -2,24 +2,45 @@
 using RepoVault.Application.Git;
 using RepoVault.CLI;
 using RepoVault.Infrastructure.Backup;
+using RepoVault.Infrastructure.Database;
 using RepoVault.Infrastructure.Services;
 //pipeline
-Console.WriteLine("Welcome to RepoVault!");
-
-Console.WriteLine("Paste your user's token here: ");
+Console.WriteLine(DateTime.Now);
+UserInteraction.ShowMenu();
+Console.WriteLine("Paste your Github user token here (don't know how to get one? Generate it here: github.com/settings/tokens): ");
 string token = Console.ReadLine();
 UserInteraction.checkUserToken(token, out GitRepository gitServices);
 
-Console.WriteLine("Here are your repositories: ");
-UserInteraction.ShowUserRepositories(gitServices, token);
+Console.WriteLine("What would you like to do? ");
+Console.WriteLine("[1] See your repositories");
+Console.WriteLine("[2] See your backups");
+var response = Console.ReadLine();
+switch (response)
+{
+    case "1":
+        Console.WriteLine("Here are your repositories: ");
+        UserInteraction.ShowUserRepositories(gitServices, token);
+        Console.WriteLine("Enter name of repository you want to see the issues for: ");
+        string repoName = Console.ReadLine();
+        Console.WriteLine("Here are the issues for "+repoName+": ");
+        await UserInteraction.ShowRepoIssues(gitServices, token, repoName);
+        Console.WriteLine("Do you want to create backup of this Repository and it's issues? (y/n): ");
+        string answer = Console.ReadLine();
+        if (answer == "y")
+        {
+            BackupRepository backupRepository = new(new GitService(token), new EncryptionService(), new RepoVaultDbRepository(new RepoVaultDbContext()));
+            backupRepository.CreateFullBackup(token, repoName);
+        }
+        break;
+    case "2":
+        Console.WriteLine("Here are your latest backups: ");
+        UserInteraction.ShowLocalBackups(gitServices, token);
+        break;
+}
 
-Console.WriteLine("Enter name of repository you want to see the issues for: ");
-string repoName = Console.ReadLine();
 
 
-Console.WriteLine("Here are the issues for "+repoName+": ");
-await UserInteraction.ShowRepoIssues(gitServices, token, repoName);
 
-BackupRepository backupRepository = new(new GitService(token), new EncryptionService());
-backupRepository.CreateFullBackup(token, repoName);
+
+
 
--- RepoVault.CLI/UserInteraction.cs
+++ RepoVault.CLI/UserInteraction.cs
@@ -1,9 +1,43 @@
-﻿using RepoVault.Infrastructure.Services;
+﻿using RepoVault.Application.Backup;
+using RepoVault.Application.Encryption;
+using RepoVault.Application.Git;
+using RepoVault.Infrastructure.Backup;
+using RepoVault.Infrastructure.Database;
+using RepoVault.Infrastructure.Services;
 
 namespace RepoVault.CLI;
 
 public class UserInteraction
 {
+    
+    public static void ShowMenu()
+    {
+        // Store the current console foreground color
+        ConsoleColor originalColor = Console.ForegroundColor;
+
+        // Set the foreground color to green
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        Console.WriteLine(@"
+ooooooooo.                                oooooo     oooo                     oooo      .   
+`888   `Y88.                               `888.     .8'                      `888    .o8   
+ 888   .d88'  .ooooo.  oo.ooooo.   .ooooo.  `888.   .8'  .oooo.   oooo  oooo   888  .o888oo 
+ 888ooo88P'  d88' `88b  888' `88b d88' `88b  `888. .8'  `P  )88b  `888  `888   888    888   
+ 888`88b.    888ooo888  888   888 888   888   `888.8'    .oP""888   888   888   888    888   
+ 888  `88b.  888    .o  888   888 888   888    `888'    d8(  888   888   888   888    888 . 
+o888o  o888o `Y8bod8P'  888bod8P' `Y8bod8P'     `8'     `Y888""""8o  `V88V""V8P' o888o   ""888"" 
+                        888                                                                     
+                       o888o                                                                                                                                                                
+");
+        Console.ForegroundColor = originalColor; 
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Visit at github.com/zoneel/RepoVault");
+        Console.WriteLine("Created by github.com/zoneel");
+        Console.ForegroundColor = originalColor;
+        Console.WriteLine("================================================================================================");
+        Console.WriteLine("Welcome to RepoVault!");
+    }
+    
     public static void checkUserToken(string token, out GitRepository gitRepository)
     {
 
@@ -30,6 +64,12 @@ public class UserInteraction
             listnum++;
         }
     }
+    
+    public static void ShowLocalBackups(GitRepository gitServices1, string s)
+    {
+        BackupRepository backupRepository = new(new GitService(s), new EncryptionService(), new RepoVaultDbRepository(new RepoVaultDbContext()));
+        backupRepository.ShowRepoBackups();
+    }
 
     public static async Task ShowRepoIssues(GitRepository gitRepository, string token, string repoName)
     {
@@ -45,6 +85,4 @@ public class UserInteraction
             Console.WriteLine($"{issue.Title} - [Created At: {issue.CreatedAt}]");
         }
     }
-    
-    
 }
